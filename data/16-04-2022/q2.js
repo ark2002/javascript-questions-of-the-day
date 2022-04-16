8b54b46a362442ca83fba92f20ba0d98f261ecce
Aryak Lahane <67817330+ark2002@users.noreply.github.com>
2022-04-16 15:58:01 +0530

    feat : 16-04-2022 completed

--- data/16-04-2022/q2.js
+++ data/16-04-2022/q2.js
@@ -3,3 +3,11 @@
 const convertToUpperCase = (string) => console.log(string.toUpperCase());
 
 convertToUpperCase("hey its me mario")
+
+//without method
+
+const convertToUpperCaseV2 = (string) =>
+  console.log([...string].map((char)=>
+    char.charCodeAt() <= 96 || char.charCodeAt() >= 123 ? char : String.fromCharCode(char.charCodeAt()-32)).join(""))
+
+convertToUpperCaseV2("hey its me mario")
